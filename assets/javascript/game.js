9c30bcdc2ff94231f24be8e9d91dc320c8aadb34
Ben Coldwell <brc9965@icloud.com>
2018-02-04 12:05:39 -0600

    updated commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,31 +2,28 @@
     // Creates an array that lists out all of the options (a-z).
     var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-    // Creating variables to hold the number of wins, losses, guesses left and guesses picked. They start at 0.
+    
     var wins = 0;
     var losses = 0;
     var guessesLeft = 9;
     var guessesSoFar = [];
 
 
-    // This function is run whenever the user presses a key.
+    // Function is ran on keypress.
     document.onkeyup = function(event) {
 
       // Determines which key was pressed.
       var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-      // Randomly chooses a choice from the options array. This is the Computer's guess.
+      // Randomly chooses a letter from the array.
       var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
       guessesSoFar.push(userGuess);
-
-      // Reworked our code from last step to use "else if" instead of lots of if statements.
-
-      // This logic determines the outcome of the game (win/loss/guesses,guesses ), and increments the appropriate number
+      
       if (userGuess == computerGuess) {
         wins++;
-    guessesLeft = 9; //reseting the guesses back to 9 so that the user can play again
-        guessesSoFar.length = 0; //this removes everything from the guesses so far array, so that the guesses from the previous round don't show
+    guessesLeft = 9; 
+        guessesSoFar.length = 0; 
     }
     else if (guessesLeft == 0){
         losses++;
@@ -34,9 +31,9 @@
         guessesSoFar.length = 0;
     }
     else if (userGuess !== computerGuess){
-        guessesLeft--; //decrementing the guesses left
+        guessesLeft--; 
     }
-        // Creating a variable to hold new HTML. HTML now keeps track of the user and computer guesses, and wins/losses/guesses left/guesses so far.
+        
         var html = "<h1>The Psychic Game</h1>" +
         "<p>Guess what letter I'm thinking of ?</p>" +
         "<p>Total Wins: " +
@@ -53,7 +50,7 @@
         "</p>"
         ;
 
-        // Set the inner HTML contents of the #game div to our html string
+        
         document.querySelector('#game').innerHTML = html;
       }
     
